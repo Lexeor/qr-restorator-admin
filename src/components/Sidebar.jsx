1b4ae178f3c0035e1d466851480d6f7f6458a43e
Lexeor <lexeor.ross@gmail.com>
2023-07-11 14:31:09 +0400

    font add + sidebar ppulated with new items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,16 +7,19 @@ function Sidebar() {
       <div className="sidebar-items-wrapper">
         <ul>
           <li>
-            <i class="ri-dashboard-line"></i>Dashboard
+            <i className="ri-dashboard-line"></i>Dashboard
           </li>
           <li className="active">
             <i className="ri-layout-grid-fill"></i>Tables
           </li>
           <li>
-            <i className="ri-draft-line"></i>Menu
+            <i className="ri-restaurant-fill"></i>Menu
           </li>
           <li>
             <i className="ri-file-chart-line"></i>Reports
+          </li>
+          <li>
+            <i className="ri-store-3-line"></i>Restaurant
           </li>
         </ul>
       </div>
