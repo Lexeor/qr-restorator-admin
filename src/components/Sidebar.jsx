4060a4e71f9c168032c920e62646b63031e670b2
Lexeor <lexeor.ross@gmail.com>
2023-07-11 13:24:44 +0400

    sidebar layout fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside>
+      <div className="sidebar-logo">Logo</div>
+      <div className="sidebar-items-wrapper">
+        <ul>
+          <li>Dashboard</li>
+          <li>Tables</li>
+          <li>Menu</li>
+          <li>Reports</li>
+        </ul>
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
