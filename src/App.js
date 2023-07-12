6fd50016f66cf57c088d568d6caeda207235132f
Lexeor <lexeor.ross@gmail.com>
2023-07-12 10:09:28 +0400

    React Router 6 add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Layout from "./pages/Layout";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
     </div>
   );
 }
